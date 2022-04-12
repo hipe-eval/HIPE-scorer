f86f50e82b3ff3e8c6532e8d1c193b1eb4a63b7e
Simon Clematide <simon.clematide@uzh.ch>
2022-04-12 17:23:49 +0200

    add setup.py and installation instructions

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HIPE-scorer',
+    version='2.0',
+    packages=['ner_evaluation'],
+    url='',
+    license='',
+    author='siclemat',
+    author_email='',
+    description=''
+)
